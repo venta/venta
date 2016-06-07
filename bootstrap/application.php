1996abdb886817dddca0320e131644cfbbcf400f
Konstantins Zukovs <konstantins@majaslapa.lv>
2016-06-07 12:31:34 +0300

    Adding routing boilerplate

--- bootstrap/application.php
+++ bootstrap/application.php
@@ -2,18 +2,26 @@
 
 use Venta\Framework\Application;
 use Venta\Framework\Contracts\ApplicationContract;
+use Venta\Routing\RoutesCollector;
 
 /*
 |--------------------------------------------------------------------------
 | Creating and returning main application class
 |--------------------------------------------------------------------------
 */
-return new class extends Application {
+return new class(realpath(__DIR__ . '/../')) extends Application
+{
     /**
      * {@inheritdoc}
      */
     public function configure()
     {
         $this->singleton(ApplicationContract::class, $this);
+
+        $this->singleton('router', function() {
+            return \Venta\Routing\createRouter(function(RoutesCollector $collector) {
+                $this->callExtensionProvidersMethod('routes', $collector);
+            });
+        });
     }
 };
