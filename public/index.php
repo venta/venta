ad1db78b4c8d24721674faf4800a5886bea893c4
Alexey Rehov <alexey.rehov@gmail.com>
2016-07-01 10:46:24 +0300

    index.php creates Venta\Request instead Zend\Diactoros\Request.

--- public/index.php
+++ public/index.php
@@ -28,6 +28,16 @@ $app = require __DIR__ . '/../bootstrap/application.php';
 /** @var \Venta\Framework\Contracts\Kernel\HttpKernelContract $kernel */
 $kernel = $app->make(\Venta\Framework\Contracts\Kernel\HttpKernelContract::class);
 
+// todo Move to kernel?
+// todo Overwrite with Venta factory ?
+$server  = \Zend\Diactoros\ServerRequestFactory::normalizeServer($_SERVER);
+$headers = \Zend\Diactoros\ServerRequestFactory::marshalHeaders($server);
+$request =
+    new \Venta\Framework\Http\Request($server, \Zend\Diactoros\ServerRequestFactory::normalizeFiles($_FILES),
+        \Zend\Diactoros\ServerRequestFactory::marshalUriFromServer($server, $headers),
+        \Zend\Diactoros\ServerRequestFactory::get('REQUEST_METHOD', $server, 'GET'), 'php://input', $headers, $_COOKIE,
+        $_GET, $_POST// todo add protocol marshalling
+    );
 /*
 |--------------------------------------------------------------------------
 | Handling request
@@ -36,7 +46,7 @@ $kernel = $app->make(\Venta\Framework\Contracts\Kernel\HttpKernelContract::class
 | This function call will make application run and handle request object
 | producing response object
 */
-$response = $kernel->handle($request = \Zend\Diactoros\ServerRequestFactory::fromGlobals());
+$response = $kernel->handle($request);
 
 /*
 |--------------------------------------------------------------------------
