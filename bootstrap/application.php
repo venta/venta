cfa59aa223df388cce44b6be4bd6c74a8f0ed390
Alexey Rehov <alexey.rehov@gmail.com>
2016-07-22 12:19:16 +0300

    Route bindings added, error handling made more verbose.

--- bootstrap/application.php
+++ bootstrap/application.php
@@ -1,5 +1,7 @@
 <?php
 
+use Abava\Container\Contract\Caller;
+use Abava\Container\Contract\Container;
 use Abava\Http\Factory\ResponseFactory;
 use Venta\Application;
 use Venta\Contracts\Application as ApplicationContract;
@@ -32,6 +34,8 @@ return new class(realpath(__DIR__ . '/../')) extends Application
 
         $this->singleton(ApplicationContract::class, $this);
         $this->singleton('app', ApplicationContract::class);
+        $this->singleton(Container::class, $this);
+        $this->singleton(Caller::class, $this);
 
         // Binding Request & Response interfaces and implementations
         // If you want to use your own classes, it's the right place to define them
@@ -39,6 +43,7 @@ return new class(realpath(__DIR__ . '/../')) extends Application
         $this->singleton(ResponseFactory::class, $this->createResponseFactory());
 
         $this->configureLogging();
+        $this->bindRouting();
     }
 
     /**
@@ -50,7 +55,16 @@ return new class(realpath(__DIR__ . '/../')) extends Application
         $this->singleton(\Whoops\RunInterface::class, $runner);
         $this->singleton('error_handler', \Whoops\RunInterface::class);
 
-        $runner->pushHandler($this->isCli() ? new PlainTextHandler : new PrettyPageHandler)->register();
+        if ($this->isCli()) {
+            // Console kernel will render exception
+            $runner->allowQuit(false);
+            $runner->sendHttpCode(false);
+            $runner->writeToOutput(false);
+        } else {
+            // Enable pretty error page for local environment only
+            $runner->pushHandler($this->isLocalEnvironment() ? new PrettyPageHandler : new PlainTextHandler);
+        }
+        $runner->register();
     }
 
     /**
@@ -63,15 +77,27 @@ return new class(realpath(__DIR__ . '/../')) extends Application
             $logger = new \Monolog\Logger('venta');
             $handler = new \Monolog\Handler\StreamHandler(__DIR__ . '/../storage/logs/app.log');
             $handler->pushProcessor(function ($record) {
-                /** @var \Psr\Http\Message\ServerRequestInterface $request */
-                $request = $this->get(\Psr\Http\Message\RequestInterface::class);
-                $server = $request->getServerParams();
-                $record['extra']['url'] = $request->getUri()->getPath();
-                $record['extra']['http_method'] = $request->getMethod();
-                $record['extra']['host'] = $request->getUri()->getHost();
-                $record['extra']['referer'] = $request->getHeader('referer');
-                $record['extra']['user_agent'] = $request->getHeader('user-agent');
-                $record['extra']['ip'] = $server['REMOTE_ADDR'] ?? null;
+                if ($this->has('request')) {
+                    // Add HTTP request related extra info
+
+                    /** @var \Psr\Http\Message\ServerRequestInterface $request */
+                    $request = $this->get('request');
+                    $server = $request->getServerParams();
+                    $record['extra']['url'] = $request->getUri()->getPath();
+                    $record['extra']['http_method'] = $request->getMethod();
+                    $record['extra']['host'] = $request->getUri()->getHost();
+                    $record['extra']['referer'] = $request->getHeader('referer');
+                    $record['extra']['user_agent'] = $request->getHeader('user-agent');
+                    $record['extra']['ip'] = $server['REMOTE_ADDR'] ?? null;
+                } elseif ($this->has('input')) {
+                    // Add cli command related extra info
+
+                    /** @var \Symfony\Component\Console\Input\InputInterface $input */
+                    $input = $this->get('input');
+                    $record['extra']['command'] = $input->getFirstArgument();
+                    $record['extra']['arguments'] = $input->getArguments();
+                    $record['extra']['options'] = $input->getOptions();
+                }
                 return $record;
             });
             $handler->setFormatter(new \Monolog\Formatter\LineFormatter());
